527a8e8137c35aa1814ea6582e18456896638ba7
jamesvincent <jamesvincent@users.noreply.github.com>
2023-06-09 14:45:59 +0100

    Update Add-SettingsCatalogAssignment.ps1
    Tweaked the code so that when amending multiple groups the original group assignments are kept.

--- Add-SettingsCatalogAssignment.ps1
+++ Add-SettingsCatalogAssignment.ps1
@@ -11,7 +11,7 @@
 [CmdletBinding()]
 Param (
     [Parameter(Mandatory = $True)] [string] $PolicyName = "",
-    [Parameter(Mandatory = $True)] [string] $TargetGroup = "",
+    [Parameter(Mandatory = $True)] [string] $TargetGroup = "Security gRoup",
     [Parameter(Mandatory = $True)] [string] $AssignmentType = ""
 )
 
@@ -43,7 +43,7 @@ $NextLink = $securityGroup."@odata.nextLink"
 while ($NextLink -ne $null){
 $securityGroup = (Invoke-RestMethod -Uri $NextLink -Headers $authHeaders -Method Get)
 $NextLink = $securityGroup."@odata.nextLink"
-$assignmentpolicy += $securityGroup.value
+$securityGroupTarget += $securityGroup.value
 }
 
 #Define Target Group variables
@@ -51,9 +51,7 @@ $TargetGroupDetails = $securityGroupTarget | Where-Object {$_.displayName -like 
 $TargetGroupName = $TargetGroupDetails.displayName
 $TargetGroupId = $TargetGroupDetails.id
 
-Write-Host "Security Group to Assign: $TargetGroupName" -ForegroundColor Yellow
-
-#Return All Settings Catalog Profiles
+#Return All Settings Catalog Policies
 $baseUri = "https://graph.microsoft.com/beta/deviceManagement"
 $restParam = @{
     Method = 'Get'
@@ -62,105 +60,119 @@ $restParam = @{
     ContentType = 'Application/json'
 }
 
-$settingsProfiles = Invoke-RestMethod @restParam
-$settingsProfileTarget = $settingsProfiles.value
-$NextLink = $settingsProfiles."@odata.nextLink"
+$settingsPolicy = Invoke-RestMethod @restParam
+$settingsPolicyTarget = $settingsPolicy.value
+$NextLink = $settingsPolicy."@odata.nextLink"
 while ($NextLink -ne $null){
-$settingsProfiles = (Invoke-RestMethod -Uri $NextLink -Headers $authHeaders -Method Get)
-$NextLink = $settingsProfiles."@odata.nextLink"
-$settingsProfileTarget += $settingsProfiles.value
+$settingsPolicy = (Invoke-RestMethod -Uri $NextLink -Headers $authHeaders -Method Get)
+$NextLink = $settingsPolicy."@odata.nextLink"
+$settingsPolicyTarget += $settingsPolicy.value
 }
 
 #Display Confirmation/Warning
-$ProfileNameArray = $settingsProfileTarget | Where-Object {$_.name -like "$PolicyName*"} 
+$SettingsPolicyDetails = $settingsPolicyTarget | Where-Object {$_.name -like "$PolicyName*"} 
 
-foreach ($i in $ProfileNameArray) {
-    Write-Host Will Assign $TargetGroupName to $i.name as $AssignmentType -ForegroundColor Green
+foreach ($i in $SettingsPolicyDetails) {
+    Write-Host Attempting to assign $TargetGroupName to $i.name as $AssignmentType -ForegroundColor Green
 }
+
 $confirmation = Read-Host "Are you sure you wish to do this? [y/n]"
 if ($confirmation -eq 'y') {
-try {
-    $TargetGroup = New-Object -TypeName psobject
+    foreach ($i in $SettingsPolicyDetails) {
+        try {
 
-    #Calculate if an Include or an Exclude assignment
-    if ($AssignmentType -eq 'Exclude') {
-        $TargetGroup | Add-Member -MemberType NoteProperty -Name '@odata.type' -Value '#microsoft.graph.exclusionGroupAssignmentTarget'
-    } elseif ($AssignmentType -eq 'Include') {
-        $TargetGroup | Add-Member -MemberType NoteProperty -Name '@odata.type' -Value '#microsoft.graph.groupAssignmentTarget'
-    }
+                            #Report existing assignments
+                            Write-Host The following security assignments exist for $i.name -ForegroundColor Yellow
+                            $assignmentsuri = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/{0}/assignments" -f $i.id
+                            $Respons = (Invoke-RestMethod -Uri $assignmentsuri -Headers $authHeaders -Method Get).value
+            
+                            foreach ($group in $($Respons.target.groupid)) {
+                                #Security Group Lookup based on supplied TargetGroup variable
+                                $baseUri = "https://graph.microsoft.com/beta"
+                                $restParam = @{
+                                    Method = 'Get'
+                                    Uri = "$baseUri/groups/$group"
+                                    Headers = $authHeaders
+                                    ContentType = 'Application/json'
+                                }
+            
+                                $securityGroupNames = Invoke-RestMethod @restParam
+                                Write-Host $securityGroupNames.displayname -ForegroundColor Yellow
+                            }
 
-    $TargetGroup | Add-Member -MemberType NoteProperty -Name 'groupId' -Value "$TargetGroupId"
+                    # Collect existing assignments of the Settings Catalog Item
+                    $assignmentsuri = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/{0}/assignments" -f $i.id
+                    $Respons = (Invoke-RestMethod -Uri $assignmentsuri -Headers $authHeaders -Method Get).value
 
-    $Target = New-Object -TypeName psobject
-    $Target | Add-Member -MemberType NoteProperty -Name 'target' -Value $TargetGroup
-    $TargetGroups = $Target
-
-    # Creating JSON object to pass to Graph
-    $Output = New-Object -TypeName psobject
-    $Output | Add-Member -MemberType NoteProperty -Name 'assignments' -Value @($TargetGroups)
-    $JSON = $Output | ConvertTo-Json -Depth 3
-
-    # Create an array of Settings Catalog Id's
-    $Id = $settingsProfileTarget | Where-Object {$_.name -like "$PolicyName*"} 
-
-    # Collect existing assignments of the Settings Catalog Item
-    foreach ($i in $Id) {
-    $assignmentsuri = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/{0}/assignments" -f $i.id
-    $Respons = (Invoke-RestMethod -Uri $assignmentsuri -Headers $authHeaders -Method Get).value
-
-    $requestBody = @{
-        assignments = @()
-    }
-    #Check if there already are groups assigned to the settings catalog item
-    if ($Respons) {
-        foreach ($group in $($Respons)) {
-            # Get group assignment
-            if ($group.target."@odata.type" -eq "#microsoft.graph.groupAssignmentTarget") {
-                # Check to see if TargetGroupId is already assigned.
-                if ($group.target.groupId -ne $TargetGroupId) {
-                    $requestBody.assignments += @{
-                        "target" = $group.target    
-                    }              
+                    $requestBody = @{
+                        assignments = @()
+                    }
+                    #Check if there already are groups assigned to the settings catalog item
+                    if ($Respons) {
+                        foreach ($group in $($Respons)) {
+                            # Get group assignment
+                            if ($group.target."@odata.type" -eq "#microsoft.graph.groupAssignmentTarget") {
+                                # Check to see if TargetGroupId is already assigned.
+                                if ($group.target.groupId -ne $TargetGroupId) {
+                                    $requestBody.assignments += @{
+                                        "target" = $group.target    
+                                    }  
+                                }
+                                # If assigned already, then report back and take no action
+                                elseif ($group.target.groupId -eq $TargetGroupId) {
+                                    Write-Host $targetgroupname '('$targetgroupid ')' is already assigned to $i.name - No action taken -ForegroundColor Red
+                                }
+                            }
+                            # Get exclusion group assignment
+                            if ($group.target."@odata.type" -eq "#microsoft.graph.exclusionGroupAssignmentTarget") {
+                                $requestBody.configurationPoliciesAssignments += @{
+                                    "target" = $group.target
+                                }
+                            } 
+                        }
+                    }
+            
+                #Append Target Group ID to already assigned groups
+                $requestBody.assignments += @{
+                    "target" = @{
+                        "@odata.type" = "#microsoft.graph.groupAssignmentTarget"
+                        "groupId"     = "$TargetGroupId"
+                    }
                 }
-                # If assigned already, then report back and take no action
-                elseif ($group.target.groupId -eq $TargetGroupId) {
-                    Write-Host This group $targetgroupname '('$targetgroupid ')' is already assigned to $i.name -ForegroundColor Red
-                    break
+                #Assign the array of Target Groups, to the Settings Catalog items
+                $restore = $requestBody | ConvertTo-Json -Depth 99
+                $assignuri = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/{0}/assign" -f $i.id
+                Invoke-RestMethod -Uri $assignuri -Headers $authHeaders -Method Post -Body $restore
+
+                #Check new assignments
+                Write-Host After changes, the following security assignments exist for $i.name -ForegroundColor Green
+                $assignmentsuri = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/{0}/assignments" -f $i.id
+                $Respons = (Invoke-RestMethod -Uri $assignmentsuri -Headers $authHeaders -Method Get).value
+
+                foreach ($group in $($Respons.target.groupid)) {
+                    #Security Group Lookup based on supplied TargetGroup variable
+                    $baseUri = "https://graph.microsoft.com/beta"
+                    $restParam = @{
+                        Method = 'Get'
+                        Uri = "$baseUri/groups/$group"
+                        Headers = $authHeaders
+                        ContentType = 'Application/json'
+                    }
+
+                    $securityGroupNames = Invoke-RestMethod @restParam
+                    Write-Host $securityGroupNames.displayname -ForegroundColor Green
                 }
-            }
-            # Get exclusion group assignment
-            if ($group.target."@odata.type" -eq "#microsoft.graph.exclusionGroupAssignmentTarget") {
-                $requestBody.configurationPoliciesAssignments += @{
-                    "target" = $group.target
-                }
-            } 
-        }
-    }
-    }
-    #Append Target Group ID to already assigned groups
-    $requestBody.assignments += @{
-        "target" = @{
-            "@odata.type" = "#microsoft.graph.groupAssignmentTarget"
-            "groupId"     = "$TargetGroupId"
-        }
-    }
-    #Assign the array of Target Groups, to the Settings Catalog items
-    foreach ($i in $Id) {
-        $restore = $requestBody | ConvertTo-Json -Depth 99
-        $assignuri = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/{0}/assign" -f $i.id
-        Invoke-RestMethod -Uri $assignuri -Headers $authHeaders -Method Post -Body $restore
-        Write-Host Successfully Assigned $TargetGroupName to $i.name as an $AssignmentType -ForegroundColor Green
-        }
-}           
-catch {
+
+            }    
+    catch {
         write-host $_.Exception.Message -f Red
         write-host $_.Exception.ItemName -f Red
         write-host
         Write-host $_.Exception.Message -LogLevel 3
         Write-host $_.Exception.ItemName -LogLevel 3
-        break
-    }     
+    }      
+}
 } elseif ($confirmation -eq 'n') {
-        write-host Assignment confirmation was declined. No action taken. -ForegroundColor Red
+        write-host It Declined
         break
 }
