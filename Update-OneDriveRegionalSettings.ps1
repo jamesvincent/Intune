ffa8fb1d85f8980e33e2043d81bf00bef21b7a18
jamesvincent <jamesvincent@users.noreply.github.com>
2023-08-24 14:35:13 +0100

    Update Update-OneDriveRegionalSettings.ps1
    Changes made

--- Update-OneDriveRegionalSettings.ps1
+++ Update-OneDriveRegionalSettings.ps1
@@ -1,69 +1,94 @@
 #Set Parameters
-$ErrorActionPreference = "Stop"
 $AdminSiteURL="https://domain-admin.sharepoint.com"
-$SiteCollAdmin = "user@domain.org"
+$SiteCollAdmin = "admin.vincent@hello.domain"
+$DELocale = "1031"
 $TimezoneName = "(UTC+01:00) Amsterdam, Berlin, Berne, Rome, Stockholm, Vienne"
-$PrimaryLocale = "1033"
-
+   
 #Connect to PnP Online to the Tenant Admin Site
 Connect-PnPOnline -Url $AdminSiteURL -Interactive
 
 #Get All OneDrive for Business Sites
-$OneDriveSites = Get-PnPTenantSite -IncludeOneDriveSites -Filter "Url -like '-my.sharepoint.com/personal/'"
+$OneDriveSites = Get-PnPTenantSite -IncludeOneDriveSites -Filter "Url -like '_domain_com'" | Where-Object {$_.LocaleID -ne $DELocale} 
 
 Try {
-#Loop through each site
+    $ErrorActionPreference = "Continue"
+        #Loop through each site
         ForEach($Site in $OneDriveSites)
         { 
-            Write-Host -f Yellow "Processing Site: "$Site.URL
-            
+            $ErrorActionPreference = "Continue"
+
+            Write-Host -f Yellow Processing Site: $Site.URL
+            Write-Host -f Gray "##########################################################"
+            Write-Host -f Gray "##########################################################"
+
             #Add Site collection Admin user permission
+            Write-Host -f Yellow Adding $SiteCollAdmin to $Site.URL
             Set-PnPTenantSite -Url $Site.URL -Owners $SiteCollAdmin
 
             #Connect to OneDrive for Business Sites for each user in scope
+            Write-Host -f Yellow Connecting to $Site.URL
             Connect-PnPOnline $Site.URL -Interactive
-
+        
             #Get the Web
+            Write-Host -f Gray "##########################################################"
+            Write-Host -f Yellow "Acquiring current settings..."
+            Write-Host -f Gray "##########################################################"
             $web = Get-PnPWeb -Includes RegionalSettings.LocaleId, RegionalSettings.TimeZones, SupportedUILanguageIds
+
+            $CurrentLocaleId = $Web.RegionalSettings.LocaleId
+            $Timezone = $Web.RegionalSettings.TimeZones | Where-Object {$_.Description -eq $TimezoneName}
+            $CurrentLanguages = $Web.SupportedUILanguageIds
 
             #Update the Region
             #Get the Locale ID
-            $CurrentLocaleId = $Web.RegionalSettings.LocaleId
-            #$Web.RegionalSettings.LocaleId
+            Write-Host -f Gray "##########################################################"
+            Write-Host -f Yellow "Current LocaleId ="$Web.RegionalSettings.LocaleId
+            Write-Host -f Yellow "Current Timezone ="$Web.RegionalSettings.TimeZone.Description
+            Write-Host -f Yellow "Current Supported Languages ="$Web.SupportedUILanguageIds
+            Write-Host -f Gray "##########################################################"
 
-            If($CurrentLocaleId -ne $Null)
+            If($CurrentLocaleId -ne $DELocale -Or $Null -ne $Timezone -Or $CurrentLanguages -notcontains $DELocale)
+            {
+            Write-Host -f Gray "##########################################################"
+            Write-Host -f Yellow "Modifying user settings, this may take time to apply."
+            Write-Host -f Gray "##########################################################"
+            }
+        
+            If($CurrentLocaleId -ne $DELocale)
             {
                 #Update Locale of the site
-                $Web.RegionalSettings.LocaleId = $PrimaryLocale
+                $Web.RegionalSettings.LocaleId = $DELocale
                 $Web.Update()
                 Invoke-PnPQuery
-                Write-host "`tLocale has been updated to $PrimaryLocale." -ForegroundColor Green
+                Write-host -f Green Locale has been updated to $DELocale
             } else {
-                Write-host "Locale not found - no action taken!" -ForegroundColor Red
+                Write-host -f Magenta Locale already defined as $DELocale. No action taken!
             }    
 
             #Update the Timezone
             #Get the time zone
-            $Timezone = $Web.RegionalSettings.TimeZones | Where {$_.Description -eq $TimezoneName}
             
-            If($Timezone -ne $Null)
+            If($Null -ne $Timezone)
             {
                 #Update time zone of the site
                 $Web.RegionalSettings.TimeZone = $Timezone
                 $Web.Update()
                 Invoke-PnPQuery
-                Write-host "Timezone Updated Successfully!" -ForegroundColor Green
+                Write-host -f Green Timezone Updated to $TimezoneName
             } else {
-                Write-host "Timezone $TimezoneName not found - no action taken!" -ForegroundColor Yellow
+                Write-host -f Magenta Timezone $TimezoneName not found!
             }
 
             #Update the Supported Site Languages
             #Get the current Languages
-            $CurrentLanguages = $Web.SupportedUILanguageIds
-            
-            If($CurrentLanguages -ne $Null)
+            If($CurrentLanguages -contains $DELocale)
             {
+                Write-host -f Magenta $DELocale is already supported, no changes required. No action taken!
+            } else {
+                Write-Host -f Green Adding support for additional languages
+                Start-Sleep -s 10
                 #Update supported languages
+                $Web.IsMultilingual = $True
                 $Web.AddSupportedUILanguage(1036)
                 $Web.AddSupportedUILanguage(1031)
                 $Web.AddSupportedUILanguage(1025)
@@ -78,6 +103,9 @@ Try {
                 $Web.AddSupportedUILanguage(1037)
                 $Web.AddSupportedUILanguage(1038)
                 $Web.AddSupportedUILanguage(1040)
+                $Web.Update()
+                Invoke-PnPQuery
+                Start-Sleep -s 1
                 $Web.AddSupportedUILanguage(1055)
                 $Web.AddSupportedUILanguage(1054)
                 $Web.AddSupportedUILanguage(1053)
@@ -94,6 +122,9 @@ Try {
                 $Web.AddSupportedUILanguage(1057)
                 $Web.AddSupportedUILanguage(1058)
                 $Web.AddSupportedUILanguage(1060)
+                $Web.Update()
+                Invoke-PnPQuery
+                Start-Sleep -s 1
                 $Web.AddSupportedUILanguage(1061)
                 $Web.AddSupportedUILanguage(1062)
                 $Web.AddSupportedUILanguage(1063)
@@ -116,16 +147,24 @@ Try {
                 $Web.AddSupportedUILanguage(1106)
                 $Web.Update()
                 Invoke-PnPQuery
-                Write-host "Supported User Interface languages added Successfully!" -ForegroundColor Green
-            } else {
-                Write-host "UI Languages not found - no action taken!" -ForegroundColor Yellow
+                $Web.Update()
+                Start-Sleep -s 8
+                Invoke-PnPQuery
+                #Start-Sleep -s 15
+                Write-host -f Green Additional language support added Successfully!
             }
 
             #Remove Site collection Admin user permission
+            Write-Host -f Gray "##########################################################"
+            Write-Host -f Yellow Removing $SiteCollAdmin from $Site.URL
             Remove-PnPSiteCollectionAdmin -Owners $SiteCollAdmin
+            Write-Host -f Gray "##########################################################"
+            Write-Host -f Gray "##########################################################"
+            Write-Host -f Green "User Modification Complete"
+            Write-Host -f Gray "##########################################################"
         }
     }
 Catch {
     $_.Exception | Out-File Set-LocaleId-Logfile.log -Append
-    Break
+    #Break
 }
